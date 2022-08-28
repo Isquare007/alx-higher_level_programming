c9dd6582b93d3d2f0c441c7cc733062527cbea63
Isquare007 <akibrahiim2019@gmail.com>
2022-08-28 21:11:22 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     lenght = len(sentence)
     if lenght > 0:
-    first_char = (lenght, sentence[0])
+        first_char = (lenght, sentence[0])
     else:
-    first_char = (lenght, None)
+        first_char = (lenght, None)
     return (first_char)
