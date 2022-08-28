4a867e91655ff90c3bc0e564be5492670669e08a
Isquare007 <akibrahiim2019@gmail.com>
2022-08-28 15:00:27 +0000

    no C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char not ord(99) or ord(67):
+            new_string += i
+    return (new_string)
