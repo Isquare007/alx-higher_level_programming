18623a92a4fbb24d15a7eb318f486a7a655d818b
Isquare007 <akibrahiim2019@gmail.com>
2022-09-20 18:58:32 +0000

    Objects & Classes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""LockedClass"""
+
+
 class LockedClass:
     """Prevent users from instantiating new LockedClass attributes apart from "first_name"""""
+    
     __slots__ = ["first_name"]
