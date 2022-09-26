4e2aca7bffaaa3d093ef345f4823ef8dabe934ca
Isquare007 <akibrahiim2019@gmail.com>
2022-09-26 20:20:08 +0000

    same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A function that returns true or false"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        return true if the object is same as class otherwise false
+    """
+
+    return (type(obj) == a_class)
+
+
