bbf82a3237c649f3d57e48f78b9d1a0a5bcc2506
Isquare007 <akibrahiim2019@gmail.com>
2022-09-27 21:57:02 +0000

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 argv.pop(0)
 try:
     des = load_from_json_file("add_item.json")
-    if des is none:
+    if des is None:
         save_to_json_file(argv, "add_item.json")
     else:
         des.extend(argv)
