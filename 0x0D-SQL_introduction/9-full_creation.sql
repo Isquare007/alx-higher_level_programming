a2fd5840a2bb8e2540b2c7cb27d077e4b0d0633b
Isquare007 <akibrahiim2019@gmail.com>
2022-11-01 18:44:20 +0000

    create full table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256),
 	score INT
 	);
-INSERT INTO second_table (id, name)
+INSERT INTO second_table (id, name, score)
 VALUES (id=1, name="John", score=10
 	id=2, name="Alex", score=3
 	id=3, name="Bob", score=14
