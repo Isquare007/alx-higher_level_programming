71e3474a4a13765b63e014f704f6d1b7491cc165
Isquare007 <akibrahiim2019@gmail.com>
2022-11-02 16:24:35 +0000

    privilages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- list all the privilages of a database user
 
-SHOW GRANT 'user_0d_1'@'localhost';
-SHOW GRANT 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
