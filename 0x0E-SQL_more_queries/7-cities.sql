50c40c6a9dbb3d676b7d113602913721596d837b
Isquare007 <akibrahiim2019@gmail.com>
2022-11-03 00:01:04 +0000

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS hbtn_0d_0.cities (
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id),
+	state_id INT NOT NULL, 
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id),
 	name VARCHAR(256) NOT NULL
 	);
