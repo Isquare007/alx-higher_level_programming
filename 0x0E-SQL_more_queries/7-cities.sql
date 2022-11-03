5fedc696e52ef162bda2de51eb50cbd5d764513a
Isquare007 <akibrahiim2019@gmail.com>
2022-11-03 00:01:57 +0000

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_0.cities (
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	state_id INT NOT NULL, 
