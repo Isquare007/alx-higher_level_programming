86aeffbfd3b4a6c46339a47b7e50dbbde31653dc
Isquare007 <akibrahiim2019@gmail.com>
2022-11-28 22:08:20 +0000

    to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const { argv } = require('process');
 
 if (isNaN(argv[2])) {
-  console.log('Not number');
+  console.log('Not a number');
 } else {
   console.log(`My number: ${parseInt(argv[2])}`);
 }
