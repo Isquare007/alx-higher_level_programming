820df16147ab05940da61d8c97d51cf83d704e10
Isquare007 <akibrahiim2019@gmail.com>
2022-11-28 20:44:08 +0000

    0-4

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.length;
 if (args < 3) {
-  consoole.log('No argument');
+  console.log('No argument');
 } else if (args === 3) {
   console.log('Argument found');
 } else {
