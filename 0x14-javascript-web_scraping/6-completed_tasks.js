80a7d7ffe66b5605946cf14a6661f6ca732739a7
Isquare007 <akibrahiim2019@gmail.com>
2023-01-17 15:09:04 +0000

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,4 +1,22 @@
 #!/usr/bin/node
 
-req = require('request');
+const req = require('request');
+const url = process.argv[2];
 
+req(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+
+  const todos = JSON.parse(body);
+  const info = {};
+
+  for (const tasks of todos) {
+    if (tasks.completed && info[tasks.userId] === undefined) {
+      info[tasks.userId] = 1;
+    } else if (tasks.completed) {
+      info[tasks.userId] += 1;
+    }
+  }
+  console.log(info);
+});
